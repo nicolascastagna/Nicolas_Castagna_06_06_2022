09876890bdcab59ade3e3bc5df4c98521fe4727d
nicolascastagna <100592012+nicolascastagna@users.noreply.github.com>
2022-06-15 15:38:19 +0200

    add headers for access API

--- back/app.js
+++ back/app.js
@@ -11,6 +11,19 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
+app.use((req, res, next) => {
+  res.setHeader("Acces-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 // enregistre « Requête reçue ! » dans la console et passe l'exécution
 app.use((req, res, next) => {
   console.log("Requête reçue !");
